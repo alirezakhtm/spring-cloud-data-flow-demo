cff4a0f92aae04cf3376468962bec862db024337
Alireza Khatami Doost <alireza.khtm@gmail.com>
2019-06-13 15:33:19 +0430

    Modules had been modified for Data Flow framework.

--- sink-service/src/main/java/com/khtm/test/springdataflow/sinkservice/SinkServiceApplication.java
+++ sink-service/src/main/java/com/khtm/test/springdataflow/sinkservice/SinkServiceApplication.java
@@ -1,10 +1,25 @@
 package com.khtm.test.springdataflow.sinkservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.annotation.StreamListener;
+import org.springframework.cloud.stream.messaging.Sink;
 
+
+@EnableBinding(Sink.class)
 @SpringBootApplication
 public class SinkServiceApplication {
+
+    private static Logger logger = LoggerFactory.getLogger(SinkServiceApplication.class);
+
+    @StreamListener(Sink.INPUT)
+    public void loggerSink(String date) {
+
+        logger.info("Received: " + date);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SinkServiceApplication.class, args);
